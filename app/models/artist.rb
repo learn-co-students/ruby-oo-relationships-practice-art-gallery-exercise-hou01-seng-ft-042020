23880348140980667a0bfb08d017e8a245141b36
Krysta110 <62581900+Krysta110@users.noreply.github.com>
2020-04-28 15:35:23 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,51 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    #returns an array of all the paintings by an artist
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    #returns an array of all the gallies that an artist has paintings in
+    paintings.map {|painting| painting.gallery}.uniq
+  end
+
+  def cities
+    #returns an array of all cities that an artist has paintings in
+    galleries.map {|gallery| gallery.city}.uniq
+
+  end
+
+  def self.total_experience
+    #returns an integer that is the total yeats of experience of all artists
+    years = 0
+    self.all.each {|artist| years += artist.years_experience}
+    return years
+  end
+
+  def self.most_prolific
+    #returns an instance of the artist with the highest amount of paintings per year of experience
+      self.all.max {|artist| artist.paintings.count/artist.years_experience}
+  end
+
+  def create_painting(title, price, gallery)
+    #given 3 arguments, creates a new painting belonging to that artist.
+    Painting.new(title, price, self, gallery)
+  end
+
+
 
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,37 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    #returns an array of all the paintings in a gallery
+    Painting.all.select {|painting| painting.gallery == self}
+  end
+
+  def artists
+    #returns an array of all artists that have a painting in a gallery
+    paintings.map {|painting| painting.artist}.uniq
+  end
+
+  def artist_names
+    #returns an array of the names of all artists that have a painting in a gallery
+    artists.map {|artist| artist.name}.uniq
+  end
+
+  def most_expensive_painting
+    #returns an instance of the most expensive painting in a gallery
+    paintings.sort {|a,b| a.price <=> b.price}
+    return paintings[0]
   end
 
 end
