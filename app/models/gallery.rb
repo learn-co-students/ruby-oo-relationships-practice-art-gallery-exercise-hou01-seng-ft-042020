f2b27cb8d8b46656a02db17b98abb0008a0fe8d3
eiphyllis <phyllisgetcity@gmail.com>
2020-04-28 16:17:13 -0400

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,16 @@
 class Gallery
-
+  @@all = []
   attr_reader :name, :city
 
   def initialize(name, city)
     @name = name
     @city = city
+    self.class.all << self 
   end
+
+  def self.all
+    @@all 
+
+  end 
 
 end
